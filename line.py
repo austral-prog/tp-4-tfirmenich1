3a71c5e3f98a9113e324d1ef5059f826a9092b3c
tfirmenich1 <tfirmenich@mail.austral.edu.ar>
2025-04-20 10:51:13 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,21 +1,28 @@
 def line():
-    A = input("Ingrese el coeficiente A: ")
-    B = input("Ingrese el coeficiente B: ")
-    X1 = input("Ingrese el coeficiente X1: ")
-    X2 = input("Ingrese el coeficiente X2: ")
-    print("El coeficiente A de su ecuación de la recta es: " + str(float(A)))
-    print("El coeficiente B de su ecuación de la recta es: " + str(float(B)))
-    print("El coeficiente X1 de su ecuación de la recta es: " + str(float(X1)))
-    print("El coeficiente X2 de su ecuación de la recta es: " + str(float(X2)))
-    A = float(A)
-    B = float(B)
-    print(f"""\nPara la siguiente ecuación:
-\tY = {A}X + {B}""")
-    Y1 = (A*X1) + B
-    Y2 = (A*X2) + B
-    print(f"""\nDados los siguientes puntos:
-\tP1 ({X1}, {Y1})
-\tP2 ({X2}, {Y2}""")
-    D = ((X2 - X1 )**2 + (Y2 - Y1)**2)**(1/2)
-    print()
-    print("la distancia entre ellos es: " + str(float(D)))
+    CoA = float(input("Ingrese el coeficiente A:"))
+    CoB = float (input ("Ingrese el coeficiente B:"))
+    CoX1 = float (input ("Ingrese el coeficiente X1:"))
+    CoX2 = float (input ("Ingrese el coeficiente X2:"))
+    Y1 = CoA * CoX1 + CoB
+    Y2 = CoA * CoX2 + CoB
+    Distancia = ((CoX2 - CoX1)**2 + (Y2 - Y1)**2)**0.5
+
+    print (f"El coeficiente A de su ecuación de la recta es: {CoA}")
+    print (f"El coeficiente B de su ecuación de la recta es: {CoB}")
+    print (f"El coeficiente X1 de su ecuación de la recta es: {CoX1}")
+    print (f"El coeficiente X2 de su ecuación de la recta es: {CoX2}")
+
+    print ("")
+
+    print ("Para la siguiente ecuación:")
+    print (f"\tY = {CoA}X + {CoB}")
+
+    print ("")
+
+    print ("Dados los siguientes puntos:")
+    print (f"\tP1 ({CoX1}, {Y1})")
+    print (f"\tP2 ({CoX2}, {Y2})")
+
+    print ()
+
+    print (f"La distancia entre ellos es: {Distancia}")
